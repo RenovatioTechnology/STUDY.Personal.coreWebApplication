207e0c3a10faf664e24859b48ee49e04abe2e55c
Jefferson Nah Jr <J-Nah@outlook.com>
2023-12-20 08:26:43 -0500

    Implementing Razor View and Razor Syntax

---
+++ coreWebApplication/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using coreWebApplication.Models;
+using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
+
+namespace coreWebApplication.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // Implement an instance of Customer Class from .Models
+        public static List<Customer> customers = new List<Customer>()
+        {
+            // For each customer we need to pass on the details an id, name and customer amount!
+            new Customer() { Id = 101, Name = "Nah", Amount = 12000},
+            new Customer() { Id = 102, Name = "King", Amount = 12000},
+        };
+        public IActionResult Index()
+        {
+            //Passing some data from Index IAction and to view
+            ViewBag.Message = "Customer Management System";
+
+            // Add Customer to our ViewBag
+            ViewBag.CustomerCount = customers.Count();
+            //Creating a customer list which will be storing the complete customers list!
+            ViewBag.CustomerList = customers;   
+
+
+            return View();
+        }
+    }
+}
